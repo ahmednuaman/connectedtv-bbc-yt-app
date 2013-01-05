8e88d606ef168c159135bb32f1be9266af75f3ec
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-05 10:08:43 +0000

    updated model

--- assets/js/app.models.js
+++ assets/js/app.models.js
@@ -9,37 +9,73 @@ and one collection:
 // strict yo
 'use strict';
 
-var VideoItemModel = Backbone.Model.extend({
-    defaults: {
-        id: '',
-        title: '',
-        description: '',
-        thumbnail: ''
+var VideoItemModel = function(data)
+{
+    // explicitly set our model's props
+    return {
+        id: data.id,
+        title: data.title,
+        description: data.description,
+        thumbnail: data.thumbnail
     }
-});
+};
 
-var VideoItemsCollection = Backbone.Collection.extend({
-    model: VideoItemModel,
+var VideoItemsCollection = function()
+{
+    // keep a ref
+    var that = this;
 
-    url: 'http://gdata.youtube.com/feeds/api/users/bbc/uploads?max-results=12&alt=json&orderby=published&format=1,5,6&callback=?',
+    // set up container for our models
+    var models = { };
 
-    // create our parse function to handle a response from the gdata api
-    // we'll be hitting: http://gdata.youtube.com/feeds/api/users/bbc/uploads?max-results=9&alt=json&orderby=published&format=1,5,6
-    parse: function(response)
+    // set our url for the data
+    var dataURL = 'http://gdata.youtube.com/feeds/api/users/bbc/uploads?max-results=12&alt=json&orderby=published&format=1,5,6&callback=';
+
+    // create a func to parse our data
+    var parse = function(response)
     {
-        var data = [ ];
+        var feed = response.feed.entry;
+        var entry;
+        var model;
 
-        // loop through our entry and map it in the same format as our VideoItemModel
-        _(response.feed.entry).forEach(function(entry)
+        for (var i = 0; i < feed.length; i++)
         {
-            data.push({
+            entry = feed[i];
+
+            model = new VideoItemModel({
                 id: entry.id.$t.replace('http://gdata.youtube.com/feeds/api/videos/', ''),
                 title: entry.title.$t,
                 description: entry.content.$t,
                 thumbnail: entry.media$group.media$thumbnail[0].url
             });
-        });
 
-        return data;
+            // add to models store
+            models[model.id] = model;
+        }
     }
-});
+
+    // create temp func name
+    var funcName = 'videoItemsCollectionParse' + (new Date()).getTime();
+
+    // create fetch function
+    this.fetch = function()
+    {
+        // create a temp reference to our parse function
+        window[funcName] = function(response)
+        {
+            return parse.apply(that, [response]);
+        };
+
+        // load the json async'ly
+        var resource = document.createElement('script');
+        resource.src = dataURL + funcName;
+        var script = document.getElementsByTagName('script')[0];
+        script.parentNode.insertBefore(resource, script);
+    };
+
+    // create a get function for retrieving a model
+    this.get = function(id)
+    {
+        return models[id];
+    }
+};

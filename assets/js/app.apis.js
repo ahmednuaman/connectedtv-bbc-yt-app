6ffbd2e41c86a8f20211481d34da76cf60b05cce
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-17 10:54:59 +0000

    testing api

--- assets/js/app.apis.js
+++ assets/js/app.apis.js
@@ -1,11 +1,27 @@
 // keep it strict
 'use strict';
 
-// declare our app apis
-var api = {
-    tv: {
+// declare our app apis, with fallback
+var api = { };
+
+try
+{
+    api.tv = {
         key: new Common.API.TVKeyValue(),
         plugin: new Common.API.Plugin(),
         widget: new Common.API.Widget()
-    }
-};
+    };
+}
+catch (e)
+{
+    api.tv = {
+        key: { },
+        plugin: { },
+        widget: {
+            sendReadyEvent: function()
+            {
+                console.log('api.tv.widget.sendReadyEvent');
+            }
+        }
+    };
+}
